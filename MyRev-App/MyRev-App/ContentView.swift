3e6ee4711181ee913733c722a4446c3d4eac322f
Joanne <joannechanliu@gmail.com>
2024-02-25 19:06:23 -0600

    Adding XCode project file

---
+++ MyRev-App/MyRev-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyRev-App
+//
+//  Created by Joanne Liu on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyRev-App/MyRev-App/MyRev_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRev_AppApp.swift
+//  MyRev-App
+//
+//  Created by Joanne Liu on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRev_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
